60c870fccd713036d2dc21f2f3905e21fd0077db
Sivaganesh T <thirusivaganesh2007@gmail.com>
2026-09-21 01:23:32 +0530

    Add files via upload

---
+++ Wishlist.java
@@ -0,0 +1,44 @@
+package com.ecommerce.model;
+
+public class Wishlist {
+
+    private int id;
+    private int userId;
+    private int productId;
+    private Product product;
+
+    public Wishlist() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
