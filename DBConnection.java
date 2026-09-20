4a11e43534d9e6e5e104d75054dba1cca318f4ab
Sivaganesh T <thirusivaganesh2007@gmail.com>
2026-09-21 01:28:33 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,31 @@
+package com.ecommerce.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/sivaganesh_martt_db";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "sivaganesh1975@69";
+
+    public static Connection getConnection() throws SQLException {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            return DriverManager.getConnection(
+                    URL,
+                    USER,
+                    PASSWORD
+            );
+
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver not found", e);
+        }
+    }
+}
